440d778c962d1db06c043b0bc90cfc3a64b879ef
zeynepesen <m.zeynepesen@hotmail.com>
2021-02-20 14:14:02 +0300

    güncelleme kısmı console'da test edildi

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -12,19 +12,28 @@ namespace ConsoleUI
         {
             CarManager carManager = new CarManager(new InMemoryCarDal());
 
-            carManager.Add(new Car { Id = 6, BrandId = 2, ColorId = 7, DailyPrice = 432, ModelYear = 2009, Description = "Yeni Araba" });
+            carManager.Add(new Car { Id = 6, BrandId = 2, ColorId = 7, DailyPrice = 432, ModelYear = 2009, Description = "Yeni eklenen Araba" });
 
+            //foreach (var car in carManager.GetAll())
+            //{
+            //    Console.WriteLine(car.Description);
+            //}
+
+            Car carToDelete = carManager.GetById(1);
+            carManager.Delete(carToDelete);
+
+            //güncelleme kısmı
+            var result = carManager.GetById(5);
+            carManager.Update(result);
+            result.Description = "güncellenen araba";
+
+            //Hepsini listeleme kısmı
             foreach (var car in carManager.GetAll())
             {
                 Console.WriteLine(car.Description);
             }
 
-            Car carToDelete = carManager.GetById(1);
-            carManager.Delete(carToDelete);
-
-           
-
         }
-        
+
     } 
 }
